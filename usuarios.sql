c3d16bdd6ebb805956d0e77ecd5b78f6672704d5
Segundo <segundogaldames@gmail.com>
2021-10-02 18:03:33 -0300

    Tarea 6

---
+++ usuarios.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios(clave,activo,empleado_id) VALUES(sha1('123456'), 1, 2);
+INSERT INTO usuarios(clave,activo,empleado_id) VALUES(sha1('Kjara.123'), 1, 5);
+INSERT INTO usuarios(clave,activo,empleado_id) VALUES(sha1('Lperez.123'), 1, 4);
